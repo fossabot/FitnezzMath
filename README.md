c6e7199eb01fd8fcfa1dc85dc0b1d6d296a783a4
Gaboso <gabrielsantiago2@gmail.com>
2018-06-27 18:32:50 -0400

    Added test to WHR and Integration with TravisCI and Coveralls

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# FitnezzMath [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+# FitnezzMath [![Build Status](https://travis-ci.com/fitnezzio/FitnezzMath.svg?branch=master)](https://travis-ci.com/fitnezzio/FitnezzMath)[![Coverage Status](https://coveralls.io/repos/github/fitnezzio/FitnezzMath/badge.svg?branch=master)](https://coveralls.io/github/fitnezzio/FitnezzMath?branch=master)[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 FitnezzMath is a repository of Fitnezz.io calculus and classifications.
 
---
+++ src/test/java/com/io/fitnezz/WaistHipRatioTest.java
@@ -0,0 +1,44 @@
+package com.io.fitnezz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WaistHipRatioTest {
+
+    @Test
+    public void calculateWHR() {
+        double waist = 81.9;
+        double hip = 99;
+
+        Double waistHipRatio = WaistHipRatio.calculate(waist, hip);
+        Assert.assertEquals(0.8272727272727274D, waistHipRatio, 0.01);
+    }
+
+    @Test
+    public void calculateWHRZeroWaist() {
+        double waist = 0;
+        double hip = 99;
+
+        Double waistHipRatio = WaistHipRatio.calculate(waist, hip);
+        Assert.assertNull(waistHipRatio);
+    }
+
+    @Test
+    public void calculateWHRZeroHip() {
+        double waist = 81.9;
+        double hip = 0;
+
+        Double waistHipRatio = WaistHipRatio.calculate(waist, hip);
+        Assert.assertNull(waistHipRatio);
+    }
+
+    @Test
+    public void calculateWHRZeroBoth() {
+        double waist = 0;
+        double hip = 0;
+
+        Double waistHipRatio = WaistHipRatio.calculate(waist, hip);
+        Assert.assertNull(waistHipRatio);
+    }
+
+}
