7252787fe5b1c032a4b4d00b33fd0c5fda2d48ef
Gaboso <gabrielsantiago2@gmail.com>
2018-06-27 21:59:03 -0400

    Update README and add more unit test

---
+++ src/test/java/com/io/fitnezz/ResidualWeightTest.java
@@ -0,0 +1,29 @@
+package com.io.fitnezz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ResidualWeightTest {
+
+    @Test
+    public void calculateResidualWeightMale() {
+        double bodyWeight = 75D;
+        Double residualWeight = ResidualWeight.calculate(bodyWeight, true);
+        Assert.assertEquals(15.675D, residualWeight, 0.01);
+    }
+
+    @Test
+    public void calculateResidualWeightFemale() {
+        double bodyWeight = 75D;
+        Double residualWeight = ResidualWeight.calculate(bodyWeight, false);
+        Assert.assertEquals(18.075D, residualWeight, 0.01);
+    }
+
+    @Test
+    public void calculateResidualWeightZero() {
+        double bodyWeight = 0D;
+        Double residualWeight = ResidualWeight.calculate(bodyWeight, false);
+        Assert.assertNull(residualWeight);
+    }
+
+}
---
+++ src/test/java/com/io/fitnezz/util/UtilsTest.java
@@ -0,0 +1,37 @@
+package com.io.fitnezz.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UtilsTest {
+
+    @Test
+    public void calculateFatWeight(){
+        double bodyWeight = 75D;
+        double fatPercentage = 15D;
+
+        double fatWeight = Utils.calculateFatWeight(bodyWeight, fatPercentage);
+        Assert.assertEquals(11.25D, fatWeight, 0.01);
+    }
+
+    @Test
+    public void calculateLeanBodyMass(){
+        double bodyWeight = 75D;
+        double fatWeight = 11.25D;
+
+        double leanBodyMass = Utils.calculateLeanBodyMass(bodyWeight, fatWeight);
+        Assert.assertEquals(63.75D, leanBodyMass, 0.01);
+    }
+
+    @Test
+    public void calculateMuscleWeight(){
+        double bodyWeight = 75D;
+        double fatWeight = 11.25D;
+        double boneWeight = 10.12D;
+        double residualWeight = 18.08D;
+
+        double leanBodyMass = Utils.calculateMuscleWeight(bodyWeight, fatWeight, boneWeight, residualWeight);
+        Assert.assertEquals(35.55D, leanBodyMass, 0.01);
+    }
+
+}
