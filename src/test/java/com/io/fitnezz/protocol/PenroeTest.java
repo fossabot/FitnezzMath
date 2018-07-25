b18d441740a5a99bacafa3ad31e572a788e8bb07
Gaboso <gabrielsantiago2@gmail.com>
2018-07-25 17:28:53 -0400

    Fixed and Added tests to Penroe

--- src/test/java/com/io/fitnezz/protocol/PenroeTest.java
+++ src/test/java/com/io/fitnezz/protocol/PenroeTest.java
@@ -1,7 +1,43 @@
 package com.io.fitnezz.protocol;
 
 import com.io.fitnezz.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class PenroeTest extends BaseTest<Penroe> {
+
+    public PenroeTest() {
+        this.clazz = Penroe.class;
+    }
+
+    @Test
+    public void calculateFatPercentageForMaleZeroLBM() {
+        double bodyWeight = 75D;
+        double leanBodyMass = 0;
+        boolean isMaleSex = true;
+
+        Double fatPercentage = Penroe.fatPercentage(bodyWeight, 0, leanBodyMass, 0, 0, isMaleSex);
+        Assert.assertNull(fatPercentage);
+    }
+
+    @Test
+    public void calculateFatPercentageForMaleZeroBW() {
+        double bodyWeight = 0;
+        double leanBodyMass = 32D;
+        boolean isMaleSex = true;
+
+        Double fatPercentage = Penroe.fatPercentage(bodyWeight, 0, leanBodyMass, 0, 0, isMaleSex);
+        Assert.assertNull(fatPercentage);
+    }
+
+    @Test
+    public void calculateFatPercentageForMaleZeroBoth() {
+        double bodyWeight = 0;
+        double leanBodyMass = 0D;
+        boolean isMaleSex = true;
+
+        Double fatPercentage = Penroe.fatPercentage(bodyWeight, 0, leanBodyMass, 0, 0, isMaleSex);
+        Assert.assertNull(fatPercentage);
+    }
 
 }
