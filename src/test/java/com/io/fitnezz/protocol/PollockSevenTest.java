cfc2c389983b1661f008592f0b06562dee040ce8
Gaboso <gabrielsantiago2@gmail.com>
2018-10-08 23:46:10 -0300

    Added tests to Pollock Seven Protocol

--- src/test/java/com/io/fitnezz/protocol/PollockSevenTest.java
+++ src/test/java/com/io/fitnezz/protocol/PollockSevenTest.java
@@ -10,8 +10,10 @@ public class PollockSevenTest extends BaseTest<PollockSeven> {
         this.clazz = PollockSeven.class;
     }
 
+    //Sum
+
     @Test
-    public void sumAll() {
+    public void sum() {
         double triceps = 10D;
         double subscapular = 30D;
         double axillary = 26D;
@@ -134,6 +136,90 @@ public class PollockSevenTest extends BaseTest<PollockSeven> {
 
         Double sum = PollockSeven.sum(triceps, subscapular, axillary, suprailiac, abdominal, thighPollock, chest);
         Assert.assertNull(sum);
+    }
+
+    //Fat Percentage Male
+
+    @Test
+    public void FatPercentageMale() {
+        int age = 19;
+        double sum = 148D;
+        boolean isMale = true;
+
+        Double fatPercentage = PollockSeven.fatPercentage(age, sum, isMale);
+        Assert.assertEquals(19.554049057527223D, fatPercentage, DELTA);
+    }
+
+    @Test
+    public void FatPercentageMaleZeroAge() {
+        int age = 0;
+        double sum = 148D;
+        boolean isMale = true;
+
+        Double fatPercentage = PollockSeven.fatPercentage(age, sum, isMale);
+        Assert.assertNull(fatPercentage);
+    }
+
+    @Test
+    public void FatPercentageMaleZeroSum() {
+        int age = 19;
+        double sum = 0D;
+        boolean isMale = true;
+
+        Double fatPercentage = PollockSeven.fatPercentage(age, sum, isMale);
+        Assert.assertNull(fatPercentage);
+    }
+
+    @Test
+    public void FatPercentageMaleZeroAll() {
+        int age = 0;
+        double sum = 0D;
+        boolean isMale = true;
+
+        Double fatPercentage = PollockSeven.fatPercentage(age, sum, isMale);
+        Assert.assertNull(fatPercentage);
+    }
+
+    //Fat Percentage Female
+
+    @Test
+    public void FatPercentageFemale() {
+        int age = 19;
+        double sum = 148D;
+        boolean isMale = false;
+
+        Double fatPercentage = PollockSeven.fatPercentage(age, sum, isMale);
+        Assert.assertEquals(27.19497735608618D, fatPercentage, DELTA);
+    }
+
+    @Test
+    public void FatPercentageFemaleZeroAge() {
+        int age = 0;
+        double sum = 148D;
+        boolean isMale = false;
+
+        Double fatPercentage = PollockSeven.fatPercentage(age, sum, isMale);
+        Assert.assertNull(fatPercentage);
+    }
+
+    @Test
+    public void FatPercentageFemaleZeroSum() {
+        int age = 19;
+        double sum = 0D;
+        boolean isMale = false;
+
+        Double fatPercentage = PollockSeven.fatPercentage(age, sum, isMale);
+        Assert.assertNull(fatPercentage);
+    }
+
+    @Test
+    public void FatPercentageFemaleZeroAll() {
+        int age = 0;
+        double sum = 0D;
+        boolean isMale = false;
+
+        Double fatPercentage = PollockSeven.fatPercentage(age, sum, isMale);
+        Assert.assertNull(fatPercentage);
     }
 
 }
