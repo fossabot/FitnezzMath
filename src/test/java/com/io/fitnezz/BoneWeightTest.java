82b054f2e02d2e4d950da2958632d0e975b0f829
Gaboso <gabrielsantiago2@gmail.com>
2018-10-17 22:33:26 -0300

    Added tests to Bone Weight

---
+++ src/test/java/com/io/fitnezz/BoneWeightTest.java
@@ -0,0 +1,52 @@
+package com.io.fitnezz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BoneWeightTest extends BaseTest<BoneWeight> {
+
+    public BoneWeightTest(){
+        this.clazz = BoneWeight.class;
+    }
+
+    @Test
+    public void calculateBoneWeight() {
+        double height = 1.69D;
+        double biStyloid = 5.2D;
+        double bicondylarFemur = 9.2D;
+
+        Double boneWeight = BoneWeight.calculate(height, biStyloid, bicondylarFemur);
+        Assert.assertEquals(10.12035246033729D, boneWeight, DELTA);
+    }
+
+    @Test
+    public void calculateBoneWeightZeroHeight() {
+        double height = 0D;
+        double biStyloid = 5.2D;
+        double bicondylarFemur = 9.2D;
+
+        Double boneWeight = BoneWeight.calculate(height, biStyloid, bicondylarFemur);
+        Assert.assertNull(boneWeight);
+    }
+
+    @Test
+    public void calculateBoneWeightZeroBiStyloid() {
+        double height = 1.69D;
+        double biStyloid = 0D;
+        double bicondylarFemur = 9.2D;
+
+        Double boneWeight = BoneWeight.calculate(height, biStyloid, bicondylarFemur);
+        Assert.assertNull(boneWeight);
+    }
+
+    @Test
+    public void calculateBoneWeightZeroBicondylarFemur() {
+        double height = 1.69D;
+        double biStyloid = 5.2D;
+        double bicondylarFemur = 0D;
+
+        Double boneWeight = BoneWeight.calculate(height, biStyloid, bicondylarFemur);
+        Assert.assertNull(boneWeight);
+    }
+
+}
